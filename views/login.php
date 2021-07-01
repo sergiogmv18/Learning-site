6f1691d729b4a279b1d1e0ea10825e91aa5a8535
guispark <guispark97@icloud.com>
2021-07-01 16:39:27 -0300

    ini

--- views/login.php
+++ views/login.php
@@ -9,6 +9,7 @@
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link type="text/css" rel="stylesheet" href="../node_modules/materialize-css/dist/css/materialize.min.css" media="screen,projection" />
     <title>Login</title>
+
 </head>
 <body>
     <div>
