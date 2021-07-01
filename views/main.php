d5b01bb4e1af0b4cd623d2c3ec44ac12e6f2c3bf
sergiogmv18 <sergiogmv18@gmail.com>
2021-07-01 17:56:35 -0300

    Link materialize and jquery to use

--- views/main.php
+++ views/main.php
@@ -1,20 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.5.0/css/swiper.min.css" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link type="text/css" rel="stylesheet" href="../node_modules/materialize-css/dist/css/materialize.min.css" media="screen,projection" />
+  <title>Document</title>
 </head>
+
 <body>
-<?php include '../component/header.php';?>
+  <?php include '../component/header.php'; ?>
 
 
-<?php include '../component/skirting.php';?>  
-<script>
-$(document).ready(function(){
-    $('.sidenav').sidenav();
-  });
+
+  <?php include '../component/skirting.php'; ?>
+  <script src="../node_modules/jquery/dist/jquery.min.js"></script>
+  <script type="text/javascript" src="../node_modules/materialize-css/dist/js/materialize.min.js"></script>
+  <script>
+    $(document).ready(function() {
+      $('.sidenav').sidenav();
+    });
   </script>
 </body>
+
 </html>
