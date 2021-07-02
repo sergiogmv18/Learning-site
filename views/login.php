4d39b786f0b75dda0a8adb1f4433c6b573742827
guispark <guispark97@icloud.com>
2021-07-01 23:30:51 -0300

    ImageLoginModified

--- views/login.php
+++ views/login.php
@@ -10,64 +10,11 @@
     <link type="text/css" rel="stylesheet" href="../style/css/general/input.css" media="screen,projection" />
     <?php include '../route/routecss.php'; ?>
     <title>Login</title>
-<<<<<<< Updated upstream
-=======
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-=======
->>>>>>> Stashed changes
-    <style>
-        body{
-            font-family: Arial, Helvetica, sans-serif;
-            background-image: linear-gradient(45deg, black,cyan);
-
-        }
-        div{
-            background-color: rgba(0, 0, 0, 0.8);
-            position:absolute;
-            top: 50%;
-            left: 50%;
-            transform: translate(-50%, -50%);
-            padding: 80px;
-            border-radius: 15px;
-            color: white;
-
-        }
-        input{
-            padding: 15px;
-            border: none;
-            outline: none;
-            font-size: 15px;
-        }
-        button{
-            background-color: dodgerblue;
-            border: none;
-            padding: 15px;
-            width: 100%;
-            color:white;
-            border-radius: 10px;
-            font-size: 15px;
-            cursor:pointer;
-        }
-        button:hover{
-        background-color: deepskyblue;
-        }
-    </style>
-   </head>
-<body>
-    <div>
-        <h1>Login</h1>
-        <input type="text" placeholder="Usuário">
-        <br><br>
-        <input type="password" placeholder="Senha">
-        <br><br>
-        <button>Acessar</button>
-=======
->>>>>>> Stashed changes
 </head>
 
 <body ng-app="mainModule" ng-controller="mainController">
     <div id="login-page" class="row">
+        
         <div class="col s12 z-depth-6 card-panel">
             <form class="login-form">
                 <div class="row">
@@ -112,4 +59,5 @@
     <script src="../style/js/login.js"></script>
     <?php include '../route/routejs.php'; ?>
 </body>
+
 </html>
