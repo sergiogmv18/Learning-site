ccafe16ea719ef7a230f5f3c55dbedd607c28c94
sergiogmv18 <sergiogmv18@gmail.com>
2021-07-01 21:26:37 -0300

    solve problem conflict of login

--- views/login.php
+++ views/login.php
@@ -10,54 +10,6 @@
     <link type="text/css" rel="stylesheet" href="../style/css/general/input.css" media="screen,projection" />
     <?php include '../route/routecss.php'; ?>
     <title>Login</title>
-<<<<<<< HEAD
-    <style>
-        body{
-            font-family: Arial, Helvetica, sans-serif;
-            background-image: linear-gradient(45deg, black,cyan);
-
-        }
-        div{
-            background-color: rgba(0, 0, 0, 0.8);
-            position:absolute;
-            top: 50%;
-            left: 50%;
-            transform: translate(-50%, -50%);
-            padding: 80px;
-            border-radius: 15px;
-            color: white;
-
-        }
-        input{
-            padding: 15px;
-            border: none;
-            outline: none;
-            font-size: 15px;
-        }
-        button{
-            background-color: dodgerblue;
-            border: none;
-            padding: 15px;
-            width: 100%;
-            color:white;
-            border-radius: 10px;
-            font-size: 15px;
-            cursor:pointer;
-        }
-        button:hover{
-        background-color: deepskyblue;
-        }
-    </style>
-   </head>
-<body>
-    <div>
-        <h1>Login</h1>
-        <input type="text" placeholder="Usuário">
-        <br><br>
-        <input type="password" placeholder="Senha">
-        <br><br>
-        <button>Acessar</button>
-=======
 </head>
 
 <body ng-app="mainModule" ng-controller="mainController">
@@ -101,14 +53,10 @@
                 </div>
             </form>
         </div>
->>>>>>> 89094569d77529a819900d33ada2df55fe6d4279
     </div>
 
     <script src="../style/js/login.js"></script>
     <?php include '../route/routejs.php'; ?>
 </body>
-<<<<<<< HEAD
-=======
 
->>>>>>> 89094569d77529a819900d33ada2df55fe6d4279
 </html>
